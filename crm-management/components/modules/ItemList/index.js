6c0c4ba9d700e9800b127afec1bd81fcb1643bec
RASAM KAMYAR <rasamkamyar@gmail.com>
2024-04-06 12:58:17 +0330

    handle change and delete

--- crm-management/components/modules/ItemList/index.js
+++ crm-management/components/modules/ItemList/index.js
@@ -10,8 +10,25 @@ function ItemList({ form, setForm }) {
     console.log(products);
   };
 
-  const changeHandler = () => {};
-  const deleteHandler = () => {};
+  const changeHandler = (e, index) => {
+    const { name, value } = e.target;
+    const newProducts = [...products];
+    newProducts[index][name] = value;
+    setForm({
+      ...form,
+      products: newProducts,
+    });
+  };
+
+  const deleteHandler = (index) => {
+    const newProducts = [...products];
+    newProducts.splice(index, 1);
+    setForm({
+      ...form,
+      products: newProducts,
+    });
+  };
+
   return (
     <div className="item-list">
       <h4>Purchased Products</h4>
@@ -22,7 +39,7 @@ function ItemList({ form, setForm }) {
             label="Product Name"
             type="text"
             value={product.name}
-            onChange={changeHandler}
+            onChange={(e) => changeHandler(e, index)}
           />
           <div>
             <FormInput
@@ -30,17 +47,17 @@ function ItemList({ form, setForm }) {
               label="Price"
               type="text"
               value={product.price}
-              onChange={changeHandler}
+              onChange={(e) => changeHandler(e, index)}
             />
             <FormInput
               name="qty"
               label="Qty"
               type="number"
               value={product.qty}
-              onChange={changeHandler}
+              onChange={(e) => changeHandler(e, index)}
             />
           </div>
-          <button onClick={deleteHandler}>Remove Item</button>
+          <button onClick={() => deleteHandler(index)}>Remove Item</button>
         </div>
       ))}
       <button onClick={addHandler}>Add Item</button>
